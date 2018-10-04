ee482488742dfbeed701bf64a4e6261704fb245c
Anuj Panchal <anujpan1@gmail.com>
2018-10-04 18:12:36 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,6 +10,8 @@ def select_winner(passengers)
   winner = ""
   
   passengers.each do |suite, name|
-    if
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
   end
 end
